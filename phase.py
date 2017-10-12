66d0435f153876998ca93e061e2bd90d7b2d9c46
TheFlash98 <mastermind.sarthak@gmail.com>
2017-10-12 21:01:25 +0530

    Add comments phase diagram png file

--- phase.py
+++ phase.py
@@ -9,17 +9,19 @@ def main():
 	plt.title('Fe Phase Diagrams')
 	plt.ylabel('X_CO')
 	plt.xlabel('Temperature')
-
+	
+	#Fe -> FeO
 	k = np.exp((-17200+(21*x))/(8.314*x))
 	y = k/(1+k)
 	plt.plot(x,y,"-")
-	#plt.show()
 
+	#Fe -> Fe3O4
 	k = np.exp((-21400+(33.8*x))/(8.314*x))
 	y = np.power(k,0.25)/(1+np.power(k,0.25))
 	plt.plot(x,y,"-")
 	#plt.show()
 
+	#FeO -> Fe3O4s
 	k = np.exp((30200+(-29.2*x))/(8.314*x))
 	y = k/(1+k)
 	plt.plot(x,y,"-")
